dcf918f9d6bd33930af06804cdb503b5b5c223d2
CimonHe <2895325697@qq.com>
2020-12-10 19:23:32 +0800

    new verson

--- WORK2/src/work1.java
+++ WORK2/src/work1.java
@@ -2,24 +2,25 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Objects;
-//编译的警告只剩下未使用的get和set方法以及异常类的构造函数和未使用的接口方法
-public class work1{
+
+//编译的警告只剩下未使用的get和set方法以及未使用的异常类的无参数构造函数和未使用的抽象接口方法
+public class work1 {
     public static void main(String[] args) {
-        West2FriedChickenRestaurant west2FriedChickenRestaurant=new West2FriedChickenRestaurant();
+        West2FriedChickenRestaurant west2FriedChickenRestaurant = new West2FriedChickenRestaurant();
         west2FriedChickenRestaurant.setBalance(50);//----------设置炸鸡店余额为50
         //-----------------------初始化啤酒果汁列表
-        LocalDate tempLocalDate=LocalDate.of(2020,12,8);
-        west2FriedChickenRestaurant.beerList.add(new Beer("百威",3,tempLocalDate,(float) 2.56));
-        west2FriedChickenRestaurant.beerList.add(new Beer("科罗娜",5,tempLocalDate,(float) 3.28));
-        west2FriedChickenRestaurant.juiceList.add(new Juice("珍珠奶茶",3,tempLocalDate));
+        LocalDate tempLocalDate = LocalDate.of(2020, 12, 8);
+        west2FriedChickenRestaurant.beerList.add(new Beer("百威", 3, tempLocalDate, (float) 2.56));
+        west2FriedChickenRestaurant.beerList.add(new Beer("科罗娜", 5, tempLocalDate, (float) 3.28));
+        west2FriedChickenRestaurant.juiceList.add(new Juice("珍珠奶茶", 3, tempLocalDate));
         System.out.println("测试过期函数************************************************************************");
-        Beer beer=new Beer("百威",3,tempLocalDate,(float) 2.56);
-        System.out.println("测试2020年12月8号生产的啤酒是否过期:"+beer.isExpired());
-        tempLocalDate=LocalDate.of(2020,9,10);
-        beer=new Beer("百威",3,tempLocalDate,(float) 2.56);
-        System.out.println("测试2020年11月9号生产的啤酒是否过期:"+beer.isExpired());
+        Beer beer = new Beer("百威", 3, tempLocalDate, (float) 2.56);
+        System.out.println("测试2020年12月8号生产的啤酒是否过期:" + beer.isExpired());
+        tempLocalDate = LocalDate.of(2020, 9, 10);
+        beer = new Beer("百威", 3, tempLocalDate, (float) 2.56);
+        System.out.println("测试2020年11月9号生产的啤酒是否过期:" + beer.isExpired());
         System.out.println("测试售卖函数以及异常抛出IngredientSortOutException**************************************");
-        try{
+        try {
             System.out.println("套餐一售卖中……………………");
             west2FriedChickenRestaurant.sellSetMeal("套餐一");
             System.out.println("套餐一售卖成功");
@@ -29,32 +30,28 @@ public class work1{
             System.out.println("套餐三售卖中……………………");
             west2FriedChickenRestaurant.sellSetMeal("套餐三");
             System.out.println("套餐三售卖成功");
-        }
-        catch (IngredientSortOutException e)
-        {
+        } catch (IngredientSortOutException e) {
             System.out.print("售卖失败（捕获异常）：");
             System.out.println(e.getMessage());
         }
         System.out.println("测试批量进货函数以及异常抛出OverdraftBalanceException*************************************");
-        try{
-            System.out.println("西二炸鸡店余额为："+west2FriedChickenRestaurant.getBalance());
-            tempLocalDate=LocalDate.of(2020,12,8);
+        try {
+            System.out.println("西二炸鸡店余额为：" + west2FriedChickenRestaurant.getBalance());
+            tempLocalDate = LocalDate.of(2020, 12, 8);
             System.out.println("批量进货中……………………");
-            west2FriedChickenRestaurant.bulkPurchase(new Juice("珍珠奶茶",3,tempLocalDate));
+            west2FriedChickenRestaurant.bulkPurchase(new Juice("珍珠奶茶", 3, tempLocalDate));
             System.out.println("批量进货成功");
             System.out.println("批量进货中……………………");
-            west2FriedChickenRestaurant.bulkPurchase(new Juice("珍珠奶茶",3,tempLocalDate));
+            west2FriedChickenRestaurant.bulkPurchase(new Juice("珍珠奶茶", 3, tempLocalDate));
             System.out.println("批量进货成功");
-        }
-        catch (OverdraftBalanceException e)
-        {
+        } catch (OverdraftBalanceException e) {
             System.out.print("进货失败（捕获异常）：");
             System.out.println(e.getMessage());
         }
     }
 }
 
-abstract  class Drinks{
+abstract class Drinks {
     protected String name;
     protected double cost;
     protected LocalDate producedDate;
@@ -67,7 +64,10 @@ abstract  class Drinks{
         this.shelfLife = shelfLife;
     }
 
-    boolean isExpired(){
+    @Override
+    public abstract String toString();
+
+    boolean isExpired() {
         return !LocalDate.now().minusDays(shelfLife).isBefore(producedDate);
     }
 
@@ -80,21 +80,43 @@ abstract  class Drinks{
     }
 }
 
-class Beer extends Drinks{
+class Beer extends Drinks {
     float concentration;
+
     public Beer(String name, double cost, LocalDate producedDate, float concentration) {
         super(name, cost, producedDate, 30);
         this.concentration = concentration;
     }
-}
 
-class Juice extends  Drinks{
-    public Juice(String name, double cost, LocalDate producedDate) {
-        super(name, cost, producedDate, 2);
+    @Override
+    public String toString() {
+        return "啤酒{" +
+                "名字：'" + name + '\'' +
+                ", 花费：" + cost +
+                ", 生产日期：" + producedDate +
+                ", 保质期：" + shelfLife +
+                ", 酒精度：" + concentration +
+                '}';
     }
 }
 
-class SetMeal{
+class Juice extends Drinks {
+    public Juice(String name, double cost, LocalDate producedDate) {
+        super(name, cost, producedDate, 2);
+    }
+
+    @Override
+    public String toString() {
+        return "果汁{" +
+                "名字：'" + name + '\'' +
+                ", 成本：" + cost +
+                ", 生产日期：" + producedDate +
+                ", 保质期：" + shelfLife +
+                '}';
+    }
+}
+
+class SetMeal {
     String name;
     double price;
     String friedChickenName;
@@ -145,8 +167,8 @@ class SetMeal{
 
     @Override
     public String toString() {
-        return "套餐：" +
-                "套餐名：'" + name + '\'' +
+        return "套餐{" +
+                "名字：" + name + '\'' +
                 ", 价格：" + price +
                 ", 炸鸡名：'" + friedChickenName + '\'' +
                 ", 饮料：" + drinks +
@@ -167,50 +189,70 @@ class SetMeal{
     }
 }
 
-interface FriedChickenRestaurant{
+interface FriedChickenRestaurant {
     void sellSetMeal(String name);
+
     void bulkPurchase(Drinks drinks);
 }
 
-class West2FriedChickenRestaurant implements FriedChickenRestaurant{
+class IngredientSortOutException extends RuntimeException {
+    public IngredientSortOutException() {
+    }
+
+    public IngredientSortOutException(String message) {
+        super(message);
+    }
+}
+
+class OverdraftBalanceException extends RuntimeException {
+    public OverdraftBalanceException() {
+    }
+
+    public OverdraftBalanceException(String message) {
+        super(message);
+    }
+
+}
+
+class West2FriedChickenRestaurant implements FriedChickenRestaurant {
     private int balance;
-    LinkedList<Beer> beerList=new LinkedList<>();
-    LinkedList <Juice> juiceList=new LinkedList<>();
-    static ArrayList <SetMeal> setMealList=new ArrayList <>();
+    LinkedList<Beer> beerList = new LinkedList<>();
+    LinkedList<Juice> juiceList = new LinkedList<>();
+    static ArrayList<SetMeal> setMealList = new ArrayList<>();
     //********************************************************************************************************************
-    //由于ArrayList查询快  而增加删除慢 所以适用于存放 初始化不改变 并且需要查询的套餐列表
+    //由于ArrayList查询快  而增加删除慢 所以适用于存放 初始化不改变、并且需要查询的套餐列表
     //而由于LinkedList查询慢  而增加删除快 所以适用于经常增删的饮料列表
     //********************************************************************************************************************
-    public static final int BULLFINCHES =10;//一次进货饮料的数量
+    public static final int BULLFINCHES = 10;//一次进货饮料的数量
+
     static {
-        LocalDate tempLocalDate=LocalDate.of(2020,11,20);
-        setMealList.add(new SetMeal("套餐一",30,"韩式炸鸡",new Beer("百威",3,tempLocalDate,(float) 2.56)));
-        tempLocalDate=LocalDate.of(2020,11,25);
-        setMealList.add(new SetMeal("套餐二",30,"美式炸鸡",new Beer("科罗娜",5,tempLocalDate,(float) 3.28)));
-        setMealList.add(new SetMeal("套餐三",30,"美式炸鸡",new Juice("珍珠奶茶",5,tempLocalDate)));
+        LocalDate tempLocalDate = LocalDate.of(2020, 11, 20);
+        setMealList.add(new SetMeal("套餐一", 30, "韩式炸鸡", new Beer("百威", 3, tempLocalDate, (float) 2.56)));
+        tempLocalDate = LocalDate.of(2020, 11, 25);
+        setMealList.add(new SetMeal("套餐二", 30, "美式炸鸡", new Beer("科罗娜", 5, tempLocalDate, (float) 3.28)));
+        setMealList.add(new SetMeal("套餐三", 30, "美式炸鸡", new Juice("珍珠奶茶", 5, tempLocalDate)));
     }
+
     private void use(Beer beer) throws IngredientSortOutException {
-        beerList.removeIf(beer1 ->beer.isExpired());//-----------先移除过期饮料
-        if (beerList.contains(beer))
-        {
+        beerList.removeIf(beer1 -> beer.isExpired());//-----------先移除过期饮料
+        if (beerList.contains(beer)) {
             beerList.remove(beer);
-        }
-        else
-            throw new IngredientSortOutException(beer.name+"啤酒卖完");
+        } else
+            throw new IngredientSortOutException(beer.name + "啤酒卖完");
     }
+
     private void use(Juice juice) throws IngredientSortOutException {
         juiceList.removeIf(juice1 -> juice.isExpired());//-----------先移除过期饮料
-        if (juiceList.contains(juice))
-        {
+        if (juiceList.contains(juice)) {
             juiceList.remove(juice);
-        }
-        else
-            throw new IngredientSortOutException(juice.name+"果汁卖完");
+        } else
+            throw new IngredientSortOutException(juice.name + "果汁卖完");
     }
+
     @Override
-    public void sellSetMeal(String name) throws IngredientSortOutException{
-        SetMeal setMeal=new SetMeal(name);
-        setMeal=setMealList.get(setMealList.indexOf(setMeal));
+    public void sellSetMeal(String name) throws IngredientSortOutException {
+        SetMeal setMeal = new SetMeal(name);
+        setMeal = setMealList.get(setMealList.indexOf(setMeal));
         if (setMeal.drinks instanceof Beer)
             this.use((Beer) setMeal.drinks);
         else
@@ -219,27 +261,21 @@ class West2FriedChickenRestaurant implements FriedChickenRestaurant{
 
     @Override
     public void bulkPurchase(Drinks drinks) throws OverdraftBalanceException {
-        if (drinks instanceof Beer)
-        {
-            if (balance>=drinks.cost* BULLFINCHES)
-            {
-                for (int i = 1; i<= BULLFINCHES; i++)
-                    beerList.add((Beer)drinks);
-                balance-=drinks.cost* BULLFINCHES;
-            }
-            else
+        if (drinks instanceof Beer) {
+            if (balance >= drinks.cost * BULLFINCHES) {
+                for (int i = 1; i <= BULLFINCHES; i++)
+                    beerList.add((Beer) drinks);
+                balance -= drinks.cost * BULLFINCHES;
+            } else
                 throw new OverdraftBalanceException("进货费用超出拥有余额");
         }
 
-        if (drinks instanceof Juice)
-        {
-            if (balance>=drinks.cost* BULLFINCHES)
-            {
-                for (int i = 1; i<= BULLFINCHES; i++)
-                    juiceList.add((Juice)drinks);
-                balance-=drinks.cost* BULLFINCHES;
-            }
-            else
+        if (drinks instanceof Juice) {
+            if (balance >= drinks.cost * BULLFINCHES) {
+                for (int i = 1; i <= BULLFINCHES; i++)
+                    juiceList.add((Juice) drinks);
+                balance -= drinks.cost * BULLFINCHES;
+            } else
                 throw new OverdraftBalanceException("进货费用超出拥有余额");
         }
     }
@@ -253,21 +289,3 @@ class West2FriedChickenRestaurant implements FriedChickenRestaurant{
     }
 }
 
-class IngredientSortOutException extends RuntimeException{
-    public IngredientSortOutException() {
-    }
-
-    public IngredientSortOutException(String message) {
-        super(message);
-    }
-}
-
-class OverdraftBalanceException extends RuntimeException{
-    public OverdraftBalanceException() {
-    }
-
-    public OverdraftBalanceException(String message) {
-        super(message);
-    }
-
-}
--- WORK2/src/work2.java
+++ WORK2/src/work2.java
@@ -2,29 +2,26 @@ import java.util.Scanner;
 
 public class work2 {
     public static void main(String[] args) {
-
-        Scanner scanner=new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("请输入x:");
         long x = scanner.nextLong();
         System.out.println("请输入n:");
         long n = scanner.nextLong();
-        long present=1;
-        Thread [] thread=new MyThread[1000010];
-        for (int i=1;i<=((long)Math.sqrt(n))+1;i++)
-        {
-            thread[i]=new MyThread(x,n,present);
+        long present = 1;
+        Thread[] thread = new MyThread[1000010];
+        for (int i = 1; i <= ((int) Math.sqrt(n)) + 1; i++) {
+            thread[i] = new MyThread(x, n, present);
             thread[i].start();
-            present+=(long)Math.sqrt(n)+2;
+            present += (long) Math.sqrt(n) + 2;
         }
-        for (int i=1;i<=((int)Math.sqrt(n))+1;i++)
-        {
+        for (int i = 1; i <= ((int) Math.sqrt(n)) + 1; i++) {
             try {
                 thread[i].join();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("1~"+n+"中包含有"+x+"的数的和为:  "+Counter.ans);
+        System.out.println("1~" + n + "中包含有" + x + "的数的和为:  " + Counter.ans);
     }
 }
 
@@ -33,29 +30,29 @@ class Counter {
     public static long ans = 0;
 }
 
-class MyThread extends Thread{
+class MyThread extends Thread {
     long present;
     long x;
     long n;
-    public MyThread(long x,long n,long present)
-    {
-        this.x=x;
-        this.n=n;
-        this.present=present;
+
+    public MyThread(long x, long n, long present) {
+        this.x = x;
+        this.n = n;
+        this.present = present;
     }
 
     @Override
     public void run() {
-        long sum=0;
-        for (long i=present;i<=present+((long)Math.sqrt(n))+1;i++)
-            if (contain(i,x)&&i<=n)
-            {
-                sum+=i;
+        long sum = 0;
+        for (long i = present; i <= present + ((long) Math.sqrt(n)) + 1; i++)
+            if (contain(i, x) && i <= n) {
+                sum += i;
             }
-        synchronized (Counter.lock){
-            Counter.ans+=sum;
+        synchronized (Counter.lock) {
+            Counter.ans += sum;
         }
     }
+
     private static boolean contain(long num, long x) {
         return String.valueOf(num).contains(String.valueOf(x));
     }
